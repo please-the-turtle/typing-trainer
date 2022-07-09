c1855abd8f74fdd1483e3026556f52bab5981019
Alexey Viklov <alexey.viklov@gmail.com>
2022-07-10 01:55:09 +0300

    Parsing: work on ParsingConsoleUI
    Parsing sources added

--- ParsingConsoleUI/Program.cs
+++ ParsingConsoleUI/Program.cs
@@ -4,46 +4,88 @@ using Parsing.JacqueFresco;
 using TypingTraining.TypingTexts;
 using Newtonsoft.Json;
 
-ParserWorker<TypingText[]> parser = new(new JacqueFrescoParser(), new JacqueFrescoParserSettings());
+string savePath = "../../../saved/";
+
+ParserWorker<TypingText[]> parserWorker;
 List<TypingText> texts = new();
+string currentSaveFileName;
 
-parser.OnNewData += (o, t) =>
+var setups = GetParsingSetups();
+
+foreach (var setup in setups)
 {
-    texts.AddRange(t);
-};
+    parserWorker = new(setup.Parser, setup.ParserSettings);
+    currentSaveFileName = setup.FileName;
+    texts.Clear();
 
-parser.OnCompleted += OnParsingCompleted;
+    parserWorker.OnNewData += (o, t) =>
+    {
+        texts.AddRange(t);
+    };
 
-parser.Start();
+    parserWorker.OnCompleted += OnParsingCompleted;
 
-while (parser.IsActive)
+    bool completed = false;
+    parserWorker.OnCompleted += (o) => completed = true;
+
+    parserWorker.Start();
+
+    while (!completed)
+    {
+        Thread.Sleep(1000);
+        Console.CursorLeft = 0;
+        Console.Write($"File: {currentSaveFileName} - Parsing.  ");
+        Thread.Sleep(1000);
+        Console.CursorLeft = 0;
+        Console.Write($"File: {currentSaveFileName} - Parsing.. ");
+        Thread.Sleep(1000);
+        Console.CursorLeft = 0;
+        Console.Write($"File: {currentSaveFileName} - Parsing...");
+    }
+
+    Console.CursorLeft = 0;
+    Console.WriteLine($"File: {currentSaveFileName} - Complete.  ");
+}
+
+List<TypingTextParsingSetup> GetParsingSetups()
 {
-    Console.SetCursorPosition(0, 0);
-    Console.WriteLine("Loading.");
-    Thread.Sleep(1000);
-    Console.SetCursorPosition(0, 0);
-    Console.WriteLine("Loading..");
-    Thread.Sleep(1000);
-    Console.SetCursorPosition(0, 0);
-    Console.WriteLine("Loading...");
+    List<TypingTextParsingSetup> setups = new();
+
+    TypingTextParsingSetup jokes = new(
+        "[RU] Jokes.type",
+        new JokesParser(),
+        new JokesParserSettings());
+    setups.Add(jokes);
+
+    TypingTextParsingSetup fresco = new(
+        "[RU] Jacue Fresco.type",
+        new JacqueFrescoParser(),
+        new JacqueFrescoParserSettings());
+    setups.Add(fresco);
+
+    return setups;
 }
 
 void OnParsingCompleted(object? obj)
 {
-    Random r = new();
-    Console.WriteLine();
-    Console.WriteLine(texts[r.Next(0, texts.Count - 1)].Content);
-    string filePath = "../../../saved/[RU] Jacue Fresco collection.type";
-    SaveTexts(filePath, texts.ToArray());
-
-    Console.WriteLine("\nParsing completed. Press any key.");
-    Console.ReadKey();
+    TrySaveTexts(currentSaveFileName, texts.ToArray());
 }
 
-void SaveTexts(string filePath, TypingText[] texts)
+bool TrySaveTexts(string fileName, TypingText[] texts)
 {
     string json = JsonConvert.SerializeObject(texts, Formatting.Indented);
-    Directory.CreateDirectory("../../../saved");
-    File.WriteAllText(filePath, json);
+
+    try
+    {
+        Directory.CreateDirectory(savePath);
+        File.WriteAllText(savePath + fileName, json);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+        return false;
+    }
+
+    return true;
 }
 
